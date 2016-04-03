792cf7c120f0021d61b8053b3d814c66fdec2312
LEONARDO <LEONARDO@DESKTOP-ODLGAO4>
2016-04-03 15:05:50 -0300

    Adição de nova tela de trabalho seu conteúdo refere a medidas de dispersão

--- src/br/com/estatistica/view/MenuPrincipalGUI.java
+++ src/br/com/estatistica/view/MenuPrincipalGUI.java
@@ -7,12 +7,14 @@ public class MenuPrincipalGUI extends javax.swing.JFrame {
     
     SeriesEstatisticasGUI SeriesEstatisticasG;
     TecnicasAmostragemGUI TecnicasAmostragemG;
+    TelaMedidasDeDispersaoGUI MedidasDeDispersaoG;
     
     public MenuPrincipalGUI() {
         initComponents();
         setExtendedState(MAXIMIZED_BOTH);
         SeriesEstatisticasG = new SeriesEstatisticasGUI();
         TecnicasAmostragemG  = new TecnicasAmostragemGUI();
+        MedidasDeDispersaoG= new TelaMedidasDeDispersaoGUI();
     }
         
     
@@ -30,6 +32,7 @@ public class MenuPrincipalGUI extends javax.swing.JFrame {
         jLabel5 = new javax.swing.JLabel();
         jLabel7 = new javax.swing.JLabel();
         jLabel8 = new javax.swing.JLabel();
+        btnMedidasDispersao = new javax.swing.JButton();
         jLabel6 = new javax.swing.JLabel();
         BtnSair = new javax.swing.JButton();
 
@@ -77,25 +80,38 @@ public class MenuPrincipalGUI extends javax.swing.JFrame {
         jLabel8.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
         jLabel8.setText("Felipe Sartori");
 
+        btnMedidasDispersao.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        btnMedidasDispersao.setText("Medidas de Dispersão");
+        btnMedidasDispersao.setPreferredSize(new java.awt.Dimension(150, 50));
+        btnMedidasDispersao.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnMedidasDispersaoActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(BtnTAmostragem, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(btnSEstatisticas, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jLabel1)
-                    .addComponent(jLabel5)
-                    .addComponent(jLabel7)
-                    .addComponent(jLabel8)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(BtnTAmostragem, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(btnSEstatisticas, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 61, Short.MAX_VALUE)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jLabel4)
-                            .addComponent(jLabel3))
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel5)
+                            .addComponent(jLabel7)
+                            .addComponent(jLabel8)))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(btnMedidasDispersao, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel3)
+                            .addComponent(jLabel4))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jLabel2)))
                 .addGap(84, 84, 84))
@@ -108,13 +124,18 @@ public class MenuPrincipalGUI extends javax.swing.JFrame {
                     .addComponent(BtnTAmostragem, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(btnSEstatisticas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel1))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(jLabel3))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jLabel4)
-                .addGap(1, 1, 1)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel3))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jLabel4))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(19, 19, 19)
+                        .addComponent(btnMedidasDispersao, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(15, 15, 15)
                 .addComponent(jLabel5)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel7)
@@ -182,6 +203,10 @@ public class MenuPrincipalGUI extends javax.swing.JFrame {
         TecnicasAmostragemG.setVisible(true);
     }//GEN-LAST:event_BtnTAmostragemActionPerformed
 
+    private void btnMedidasDispersaoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnMedidasDispersaoActionPerformed
+        MedidasDeDispersaoG.setVisible(true);
+    }//GEN-LAST:event_btnMedidasDispersaoActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -220,6 +245,7 @@ public class MenuPrincipalGUI extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton BtnSair;
     private javax.swing.JButton BtnTAmostragem;
+    private javax.swing.JButton btnMedidasDispersao;
     private javax.swing.JButton btnSEstatisticas;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
