db23a33668d87332810cc4f3b73c039dcb49f0c5
Felipe Sartori <ff.sartori@gmail.com>
2016-05-14 17:46:52 -0300

    Acréscimo da tela da tabela Discreta nova

--- src/br/com/estatistica/view/MenuPrincipalGUI.java
+++ src/br/com/estatistica/view/MenuPrincipalGUI.java
@@ -194,6 +194,7 @@ public class MenuPrincipalGUI extends javax.swing.JFrame {
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 new MenuPrincipalGUI().setVisible(true);
             }
